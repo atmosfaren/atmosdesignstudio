37e7acd76f2dadb2bb32e4a21bc43b5e649b8950
atmosfaren <168104542+atmosfaren@users.noreply.github.com>
2024-08-20 22:20:10 +0200

    scripts.js

--- scripts.js
+++ scripts.js
@@ -613,7 +613,7 @@ function initHiringForms() {
                 howDidYouFindUs: form.querySelector('select[name="how-did-you-find-us"]').value
             };
 
-            emailjs.send('atmosdesignstudio', 'atmosdesignstudio', formData)
+            emailjs.send('A-CuGtiiFvvuNPmEc', 'atmosdesignstudio', formData)
                 .then(function(response) {
                     console.log('SUCCESS!', response.status, response.text);
                     alert('Application submitted successfully!');
@@ -642,7 +642,7 @@ function initContactForm() {
             message: contactForm.querySelector('textarea[name="message"]').value
         };
 
-        emailjs.send('atmosdesignstudio', 'atmosdesignstudio', formData)
+        emailjs.send('A-CuGtiiFvvuNPmEc', 'atmosdesignstudio', formData)
             .then(function(response) {
                 console.log('SUCCESS!', response.status, response.text);
                 alert('Message sent successfully!');
